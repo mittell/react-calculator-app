727b8618ec87b21e3cec83ab316f7a7d682a1616
Chris Mittell <mittell@hotmail.co.uk>
2022-08-23 07:59:52 +0900

    feat: added button styling

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,19 @@
 import React from 'react';
 
 const Button = ({ value }) => {
-	return <button>{value}</button>;
+	const getStyleName = (btn) => {
+		const className = {
+			'=': 'equals',
+			x: 'opt',
+			'-': 'opt',
+			'+': 'opt',
+			'/': 'opt',
+		};
+
+		return className[btn];
+	};
+
+	return <button className={`${getStyleName(value)} button`}>{value}</button>;
 };
 
 export default Button;
