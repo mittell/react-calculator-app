7cdc1a319fff92ae5d3e5b4eed2aafc8c4e4c6e3
Chris Mittell <mittell@hotmail.co.uk>
2022-08-23 07:53:20 +0900

    feat: setup base ButtonBox and Button components

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
 import Screen from './components/Screen';
 import Wrapper from './components/Wrapper';
+import ButtonBox from './components/ButtonBox';
 
 import './App.css';
+import Button from './components/Button';
+
+const btnValues = [
+	['C', '+-', '%', '/'],
+	[7, 8, 9, 'x'],
+	[4, 5, 6, '-'],
+	[1, 2, 3, '+'],
+	[0, '.', '='],
+];
 
 function App() {
 	return (
 		<div>
 			<Wrapper>
 				<Screen />
-				<h1>App</h1>
+				<ButtonBox>
+					{btnValues.flat().map((btn, i) => (
+						<Button value={btn} key={i} />
+					))}
+				</ButtonBox>
 			</Wrapper>
 		</div>
 	);
