1a975d15ce6fd0ae698de4e7dce6ca91543d7a9a
Chris Mittell <mittell@hotmail.co.uk>
2022-08-23 07:43:45 +0900

    feat: added base Wrapper component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import './App.css';
+import Wrapper from './components/Wrapper';
 
 function App() {
-	return <div className='App'></div>;
+	return (
+		<div>
+			<Wrapper>
+				<h1>App</h1>
+			</Wrapper>
+		</div>
+	);
 }
 
 export default App;
