046fcb23b68bab1189b0cd8f678814c9d606007d
Chris Mittell <mittell@hotmail.co.uk>
2022-08-23 07:47:43 +0900

    feat: added base Screen component and styling

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import './App.css';
+import Screen from './components/Screen';
 import Wrapper from './components/Wrapper';
+
+import './App.css';
 
 function App() {
 	return (
 		<div>
 			<Wrapper>
+				<Screen />
 				<h1>App</h1>
 			</Wrapper>
 		</div>
---
+++ src/components/Screen.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Screen = () => {
+	return <div className='screen'>0123456</div>;
+};
+
+export default Screen;
